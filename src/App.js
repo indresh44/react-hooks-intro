87f457b39ccff4cb4da8e1b02edad26b49ca0cb1
Indresh Kumar <kindresh44@gmail.com>
2021-07-18 19:43:28 +0530

    add network status listener

--- src/App.js
+++ src/App.js
@@ -8,13 +8,18 @@ function App() {
   const [count, setCount] = useState(0)
   const [index, setIndex] = useState(0)
   const [mousePosition, setMousePosition] = useState({ x: null, y: null})
+  const [netStatus, setNetStatus] = useState(window.navigator.onLine)
 
   useEffect(() => {
     document.title = `You have clicked ${count} times`;
     window.addEventListener('mousemove', handleMouseMove);
+    window.addEventListener('online', handleOnline);
+    window.addEventListener('offline', handleOffline)
 
     return () => {
-      window.removeEventListener('mousemove', handleMouseMove)
+      window.removeEventListener('mousemove', handleMouseMove);
+      window.removeEventListener('online', handleOnline);
+      window.removeEventListener('offline', handleOffline)
     }
   }, [count]); // remove count and make it empty box, and see the page title.
 
@@ -31,6 +36,14 @@ function App() {
       x: event.pageX,
       y: event.pageY
     })
+  }
+
+  const handleOnline = event => {
+    setNetStatus(window.navigator.onLine)
+  }
+
+  const handleOffline = event => {
+    setNetStatus(window.navigator.onLine)
   }
 
   return (
@@ -51,7 +64,13 @@ function App() {
 
     <h2>Mouse Position</h2>
     {JSON.stringify(mousePosition, null, 2)}
+    <br />
+
+    <h2>Network Status</h2>
+    <p>Current network status is {netStatus ? "Online" : "Offline"}</p>
+    
     </>
+
   );
 }
 
