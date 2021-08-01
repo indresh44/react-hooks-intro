cdfdff87cfc07feaafee082cb3030513ee65c1cf
Indresh Kumar <kindresh44@gmail.com>
2021-08-01 19:46:28 +0530

    using Context with useContext Hooks

--- src/contextAndReducer/Display.js
+++ src/contextAndReducer/Display.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { UserContext } from "./Home";
 
 export default function Display() {
+  const username = useContext(UserContext);
+
   return (
     <div>
-      <UserContext.Consumer>
+      {/* Accessing state without Hooks:- */}
+      {/* <UserContext.Consumer>
         {(value) => <div>hello , {value}</div>}
-      </UserContext.Consumer>
+      </UserContext.Consumer> */}
+      {/* Accessing State using useContext Hooks */}
+      <h3>Hello, {username}</h3>
       Hello and wello
     </div>
   );
